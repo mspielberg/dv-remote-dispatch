9c13aa98a7c28073126a3b23fc968dcaaeadaa24
Miles Spielberg <miles@box.com>
2021-07-11 08:29:39 -0700

    Use real-world dimensions

--- RailTracks.cs
+++ RailTracks.cs
@@ -8,48 +8,57 @@ using UnityEngine;
 
 namespace DvMod.RemoteDispatch
 {
+    public static class World
+    {
+        private const float EARTH_CIRCUMFERENCE = 40e6f;
+        private const float DEGREES_PER_METER = 360f / EARTH_CIRCUMFERENCE;
+
+        public readonly struct Position
+        {
+            public readonly float x;
+            public readonly float z;
+
+            public Position(float x, float z)
+            {
+                this.x = x;
+                this.z = z;
+            }
+
+            public Position(Vector3 position) : this(position.x, position.z) { }
+            public Position(Transform transform) : this(transform.position) { }
+
+            public LatLon ToLatLon() => LatLon.From(this);
+        }
+
+        public readonly struct LatLon
+        {
+            public readonly float latitude;
+            public readonly float longitude;
+
+            public LatLon(float latitude, float longitude)
+            {
+                this.latitude = latitude;
+                this.longitude = longitude;
+            }
+
+            public static LatLon From(Position p) => new LatLon(DEGREES_PER_METER * p.z, DEGREES_PER_METER * p.x);
+
+            public JToken ToJson() => new JArray(latitude, longitude);
+        }
+    }
+
     public static class RailTracks
     {
         private const float SIMPLIFIED_RESOLUTION = 40f;
 
-        private struct MapBounds
-        {
-            float minX, maxX;
-            float minZ, maxZ;
+        private static IEnumerable<World.LatLon> NormalizeTrackPoints(IEnumerable<World.Position> positions) => positions.Select(p => p.ToLatLon());
 
-            public MapBounds(float minX, float maxX, float minZ, float maxZ)
-            {
-                this.minX = minX;
-                this.maxX = maxX;
-                this.minZ = minZ;
-                this.maxZ = maxZ;
-            }
+        public static IEnumerable<IEnumerable<World.LatLon>> GetNormalizedTrackCoordinates() => GetAllTrackPoints().Select(NormalizeTrackPoints);
 
-            public (float x, float z) Normalize((float x, float z) p) =>
-                (Mathf.InverseLerp(minZ, maxZ, p.z), Mathf.InverseLerp(minX, maxX, p.x));
-        }
+        public static IEnumerable<IEnumerable<World.Position>> GetAllTrackPoints(float resolution = SIMPLIFIED_RESOLUTION) =>
+             Component.FindObjectsOfType<RailTrack>().Select(track => GetTrackPoints(track, resolution));
 
-        public static (float x, float z) NormalizePosition((float x, float z) p) => Bounds.Normalize(p);
-
-        private static readonly MapBounds Bounds = new MapBounds(
-                minX: GetAllTrackPoints().SelectMany(x => x).Min(p => p.Item1),
-                maxX: GetAllTrackPoints().SelectMany(x => x).Max(p => p.Item1),
-                minZ: GetAllTrackPoints().SelectMany(x => x).Min(p => p.Item2),
-                maxZ: GetAllTrackPoints().SelectMany(x => x).Max(p => p.Item2));
-
-        private static IEnumerable<(float x, float z)> NormalizeTrackPoints(IEnumerable<(float x, float z)> rawPoints) =>
-            rawPoints.Select(NormalizePosition);
-
-        public static IEnumerable<IEnumerable<(float x, float z)>> GetNormalizedTrackCoordinates() =>
-            GetAllTrackPoints().Select(NormalizeTrackPoints);
-
-        public static IEnumerable<IEnumerable<(float x, float z)>> GetAllTrackPoints(float resolution = SIMPLIFIED_RESOLUTION)
-        {
-            foreach (var rt in Component.FindObjectsOfType<RailTrack>())
-                yield return GetTrackPoints(rt, resolution);
-        }
-
-        private static IEnumerable<(float x, float z)> GetTrackPoints(RailTrack track, float resolution = SIMPLIFIED_RESOLUTION)
+        private static IEnumerable<World.Position> GetTrackPoints(RailTrack track, float resolution = SIMPLIFIED_RESOLUTION)
         {
             var pointSet = track.GetPointSet();
             EquiPointSet simplified = EquiPointSet.ResampleEquidistant(
@@ -57,28 +66,24 @@ namespace DvMod.RemoteDispatch
                 Mathf.Min(resolution, (float)pointSet.span / 3));
 
             foreach (var pt in simplified.points)
-                yield return ((float)pt.position.x, (float)pt.position.z);
+                yield return new World.Position((float)pt.position.x, (float)pt.position.z);
         }
 
         private static readonly string trackPointJSON = JsonConvert.SerializeObject(
-            GetNormalizedTrackCoordinates().Select(trackPoints =>
-                trackPoints.Select(pt =>
-                    new JArray(pt.x, pt.z)
-                )
-            )
-        );
+            GetNormalizedTrackCoordinates().Select(trackPoints => trackPoints.Select(ll => ll.ToJson())));
 
         public static string GetTrackPointJSON() => trackPointJSON;
+    }
 
+    public static class Junctions
+    {
         private static readonly string junctionPointJSON = JsonConvert.SerializeObject(
             JunctionsSaveManager.OrderedJunctions.Select(j =>
             {
                 var moved = j.position - WorldMover.currentMove;
-                var normalized = NormalizePosition((moved.x, moved.z));
-                return new JArray(normalized.x, normalized.z);
+                return new World.Position(moved.x, moved.z).ToLatLon().ToJson();
             })
         );
-
         public static string GetJunctionPointJSON() => junctionPointJSON;
 
         public static string GetJunctionStateJSON()
