bb99a6eaeb6139cc0eefae342445cbbddb5975a5
mspielberg <9729801+mspielberg@users.noreply.github.com>
2021-07-11 08:39:28 -0700

    Indicate locomotive facing

--- main.js
+++ main.js
@@ -38,25 +38,31 @@ function updateJunctions() {
   );
 }
 
+function createCarShape(carData) {
+  const transform = `rotate(${carData.rotation + 270},100,100)`;
+  const svg = carData.isLoco
+  ? `<polygon points="50,90 50,110 140,110 150,100 140,90" transform="${transform}" fill="goldenrod" fill-opacity="50%" stroke="goldenrod" stroke-width="1%"/>`
+  : `<rect x="50" y="90" width="100" height="20" transform="${transform}" fill="magenta" fill-opacity="50%" stroke="magenta" stroke-width="1%"/>`;
+  return svg;
+}
+
 function createCarOverlay(carId, carData) {
-  const color = carData.isLoco ? 'goldenrod' : 'magenta';
-  const textRotation = ((carData.rotation + 180) % 180) - 90;
+  const textRotation = (carData.rotation % 180) - 90;
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute('id', carId)
   svg.setAttribute('xmlns', "http://www.w3.org/2000/svg");
   svg.setAttribute('viewBox', "0 0 200 200");
   svg.innerHTML =
-    `<rect x="50" y="90" width="100" height="20" transform="rotate(${carData.rotation + 90},100,100)" fill-opacity="50%" fill="${color}" stroke="${color}" stroke-width="1%"/>` +
+    createCarShape(carData) +
     `<text x="60" y="105" transform="rotate(${textRotation},100,100)" font-weight="bold">${carId}</text>`;
   return svg
 }
 
 function updateCarOverlay(carId, carData) {
-  const color = carData.isLoco ? 'goldenrod' : 'magenta';
-  const textRotation = ((carData.rotation + 180) % 180) - 90;
+  const textRotation = (carData.rotation % 180) - 90;
   const svg = document.getElementById(carId);
   svg.innerHTML =
-    `<rect x="50" y="90" width="100" height="20" transform="rotate(${carData.rotation + 90},100,100)" fill-opacity="50%" fill="${color}" stroke="${color}" stroke-width="1%"/>` +
+    createCarShape(carData) +
     `<text x="60" y="105" transform="rotate(${textRotation},100,100)" font-weight="bold">${carId}</text>`;
 }
 
