df7f29ab22a28f6fa686a75fc1340308be74ee61
mspielberg <9729801+mspielberg@users.noreply.github.com>
2021-08-24 15:50:45 -0700

    Move event publishing onto separate thread

--- EventSource.cs
+++ EventSource.cs
@@ -1,6 +1,8 @@
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using System.Threading;
 
 namespace DvMod.RemoteDispatch
 {
@@ -9,36 +11,57 @@ namespace DvMod.RemoteDispatch
         private static readonly byte[] MessagePrefix = Encoding.UTF8.GetBytes("data: ");
         private static readonly byte[] MessageTerminator = Encoding.UTF8.GetBytes("\n\n");
 
+        private const int QueueLimit = 10;
+        private static readonly BlockingCollection<string> messageQueue =
+            new BlockingCollection<string>(QueueLimit);
+
         private static readonly Dictionary<string, HttpListenerContext> activeContexts =
             new Dictionary<string, HttpListenerContext>();
 
+        static EventSource()
+        {
+            new Thread(PublisherMain).Start();
+        }
+
         public static void PublishMessage(string message)
         {
-            var deadContexts = new HashSet<string>();
-            var buf = Encoding.UTF8.GetBytes(message);
+            if (!messageQueue.TryAdd(message))
+                Main.mod?.Logger.Warning("Messages are being dropped due to one or more slow clients");
+        }
 
-            foreach (var (sessionId, context) in activeContexts)
+        private static void PublisherMain()
+        {
+            while (!messageQueue.IsCompleted)
             {
-                var stream = context.Response.OutputStream;
-                try
+                if (messageQueue.TryTake(out var message))
                 {
-                    stream.Write(MessagePrefix, 0, MessagePrefix.Length);
-                    stream.Write(buf, 0, buf.Length);
-                    stream.Write(MessageTerminator, 0, MessageTerminator.Length);
-                    stream.Flush();
-                    Main.DebugLog(() => $"Sent message to {sessionId}: {message}");
-                }
-                catch
-                {
-                    deadContexts.Add(sessionId);
-                }
-            }
+                    var deadContexts = new HashSet<string>();
+                    var buf = Encoding.UTF8.GetBytes(message);
 
-            foreach (var sessionId in deadContexts)
-            {
-                Main.DebugLog(() => $"Lost connection to EventSource subscriber: {sessionId}");
-                activeContexts[sessionId].Response.Close();
-                activeContexts.Remove(sessionId);
+                    foreach (var (sessionId, context) in activeContexts)
+                    {
+                        var stream = context.Response.OutputStream;
+                        try
+                        {
+                            stream.Write(MessagePrefix, 0, MessagePrefix.Length);
+                            stream.Write(buf, 0, buf.Length);
+                            stream.Write(MessageTerminator, 0, MessageTerminator.Length);
+                            stream.Flush();
+                            Main.DebugLog(() => $"Sent message to {sessionId}: {message}");
+                        }
+                        catch
+                        {
+                            deadContexts.Add(sessionId);
+                        }
+                    }
+
+                    foreach (var sessionId in deadContexts)
+                    {
+                        Main.DebugLog(() => $"Lost connection to EventSource subscriber: {sessionId}");
+                        activeContexts[sessionId].Response.Close();
+                        activeContexts.Remove(sessionId);
+                    }
+                }
             }
         }
 
@@ -54,8 +77,9 @@ namespace DvMod.RemoteDispatch
             Main.DebugLog(() => $"New EventSource subscriber: {sessionId}");
         }
 
-        public static void CloseAll()
+        public static void Shutdown()
         {
+            messageQueue.CompleteAdding();
             foreach (var (sessionId, context) in activeContexts)
             {
                 Main.DebugLog(() => $"Closing connection to EventSource subscriber: {sessionId}");
