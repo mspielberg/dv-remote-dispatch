bbc600209e377744bd848acd232a87fdedb8b3ca
Brian Engelhardt <lassombra@live.com>
2023-11-14 22:14:54 -0600

    Adds a bundling powershell script borrowed from derail valley umm template and also updates the readme about how to use this for convenient rapid testing.

---
+++ package.ps1
@@ -0,0 +1,28 @@
+﻿param (
+	[switch]$NoArchive,
+	[string]$OutputDirectory = $PSScriptRoot
+)
+
+Set-Location "$PSScriptRoot"
+$FilesToInclude = "info.json","bin\Debug\netstandard2.0\RemoteDispatch.dll","LICENSE"
+
+$modInfo = Get-Content -Raw -Path "info.json" | ConvertFrom-Json
+$modId = $modInfo.Id
+$modVersion = $modInfo.Version
+
+$DistDir = "$OutputDirectory/dist"
+if ($NoArchive) {
+	$ZipWorkDir = "$OutputDirectory"
+} else {
+	$ZipWorkDir = "$DistDir/tmp"
+}
+$ZipOutDir = "$ZipWorkDir/$modId"
+
+New-Item "$ZipOutDir" -ItemType Directory -Force
+Copy-Item -Force -Path $FilesToInclude -Destination "$ZipOutDir"
+
+if (!$NoArchive)
+{
+	$FILE_NAME = "$DistDir/${modId}_v$modVersion.zip"
+	Compress-Archive -Update -CompressionLevel Fastest -Path "$ZipOutDir/*" -DestinationPath "$FILE_NAME"
+}
